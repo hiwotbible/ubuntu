5386d555490c2b5002b6689236cfb05d635843bf
hiwotbible <hiwotbible@gmail.com>
2018-08-17 14:58:49 +0100

    working on Display details of shelters

---
+++ client/src/components/Shelter/ShelterCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class ShelterCard extends React.Component {
+    render() {
+      return 
+(            <div>
+                <section>
+                 <h1>
+                     'name,phoneNamber,imail'
+    
+                 </h1>
+                 </section>
+            </div>
+         )
+    }
+};
+    
+    
+
+
+export default ShelterCard;
--- client/src/containers/About/About.js
+++ client/src/containers/About/About.js
@@ -1,9 +1,14 @@
 import React from "react";
+import ShelterCard from '../../components/Shelter/ShelterCard';
 
 const About = () => (
   <div>
     <h2>ABout the shelter</h2>
+    <div>
+        <ShelterCard />
   </div>
+  </div>
+ 
 );
 
 export default About;
